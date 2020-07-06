61915cc699ea596fa4a9b7e5d5eea67424191bfa
sagonzalezp <ser55.rock@gmail.com>
2020-07-06 07:32:30 -0500

    adding more tests to de simple test file

--- cypress/integration/tutorial-examples/simple.spec.js
+++ cypress/integration/tutorial-examples/simple.spec.js
@@ -1,7 +1,17 @@
 /// <reference types="Cypress" />
 
-describe('my fist simple test with cypress', ()=>{
+describe('My fist simple test with cypress', ()=>{
     it('True should be Ture', ()=>{
         expect(true).to.equal(true)
+    })
+
+    it('5 should be 5', ()=>{
+        expect(5).to.equal(5)
+    })
+})
+
+describe('Another describe block', ()=>{
+    it('False should be false', ()=>{
+        expect(false).to.equal(false)
     })
 })
