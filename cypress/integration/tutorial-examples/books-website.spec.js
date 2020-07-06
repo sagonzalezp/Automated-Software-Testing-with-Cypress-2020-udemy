1d016097fb29f1ed9c2e080bd6f77c7d19967649
sagonzalezp <ser55.rock@gmail.com>
2020-07-06 16:47:55 -0500

    test a website with basic comands

--- cypress/integration/tutorial-examples/books-website.spec.js
+++ cypress/integration/tutorial-examples/books-website.spec.js
@@ -3,6 +3,9 @@ describe('Browser actions', ()=>{
     it("Should load book's website", ()=>{
         cy.visit('http://books.toscrape.com', {timeout: 10000})
         cy.url().should('include', 'toscrape.com')
+        cy.log('Website loaded')
+        cy.reload()
+        cy.log('After the reload')
     })
 
     it('Should click on Travel category', ()=>{
@@ -10,4 +13,14 @@ describe('Browser actions', ()=>{
         cy.get('h1').contains('Travel')
     })
     
+    it('Should display currect number of books', ()=>{
+        cy.get('.product_pod').its('length').should('eq', 11)
+        cy.get('.form-horizontal').should('contain', '11 results.')
+    })
+
+    it('Should check the price of the book', ()=>{
+        cy.get(':nth-child(22) > a').click()
+        cy.get(':nth-child(4) > .product_pod > .image_container > a > .thumbnail').click()
+        cy.get('.product_main > .price_color').should('have.text', '£23.88')
+    })
 })
