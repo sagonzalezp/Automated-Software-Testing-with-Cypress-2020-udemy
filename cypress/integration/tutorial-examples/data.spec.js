6589da22db0ea0b7a8bb087eb293eedad83d6895
sagonzalezp <ser55.rock@gmail.com>
2020-08-17 21:26:02 -0500

    how to write/read files and assertions on document

--- cypress/integration/tutorial-examples/data.spec.js
+++ cypress/integration/tutorial-examples/data.spec.js
@@ -16,4 +16,11 @@ describe('write / read data to JSON or text file', ()=>{
     it('should read and verify data from text file', ()=>{
         cy.readFile('cypress/fixtures/log.txt').should('eq', 'Hello World')
     })
+
+    it('should read and verify browser document content', ()=>{
+        cy.visit('https://www.example.com')
+        cy.wait(2000)
+        cy.document().its('contentType').should('eq', 'text/html')
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    })
 })
