051692dfd94dd3e71518ba663f78a1da957966b8
sagonzalezp <ser55.rock@gmail.com>
2020-08-17 20:37:27 -0500

    basic example of how to scrool down and up

---
+++ cypress/integration/tutorial-examples/scroll.spec.js
@@ -0,0 +1,10 @@
+describe('Scroll on the page', () =>{
+    it('should scroll down and up on the page', ()=>{
+        //TODO
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+        cy.wait(5000)
+        cy.get('[data-testid="submit-button"]').scrollIntoView()
+        cy.wait(5000)
+        cy.get('header').scrollIntoView()
+    })
+})
